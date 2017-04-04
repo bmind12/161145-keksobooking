ce860074311b83f9cc603ab6745cc770ee45cba3
Sergey R <ryabovs@yahoo.com>
2017-04-04 20:22:34 +0200

    Refactornig. Fixes made as per comments.

--- js/map.js
+++ js/map.js
@@ -1,15 +1,37 @@
 'use strict';
 
-var TITLE_LIST = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var TITLE_LIST = [
+  'Большая уютная квартира',
+  'Маленькая неуютная квартира',
+  'Огромный прекрасный дворец',
+  'Маленький ужасный дворец',
+  'Красивый гостевой домик',
+  'Некрасивый негостеприимный домик',
+  'Уютное бунгало далеко от моря',
+  'Неуютное бунгало по колено в воде'
+];
 var TYPE_LIST = ['flat', 'house', 'bungalo'];
 var CHECKIN_LIST = ['12:00', '13:00', '14:00'];
 var CHECKOUT_LIST = ['12:00', '13:00', '14:00'];
-var FEATURES_LIST = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var FEATURES_LIST = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
 var AD_LIST_LENGTH = 8;
 var MAP_CLASS = 'tokyo__pin-map';
+var MARKER_WIDTH = 56;
+var MARKER_HEIGHT = 75;
+/*
+  Перенес эти переменные наверх. Если их оставить перед функцией, они не успевают декларироваться.
+*/
 
 var adsList = generateAds();
 var adsHTML = generateAdsHTML(adsList);
+
 appendAds(adsHTML, MAP_CLASS);
 generateLodge(adsList[0]);
 
@@ -17,19 +39,20 @@ function generateAds() {
   var adList = [];
 
   for (var i = 1; i <= AD_LIST_LENGTH; i++) {
-    var imgNum = i > 9 ? i : '0' + i;
-    var offerTitle = TITLE_LIST[i / TITLE_LIST.length];
-    var offerPrice = getRandomIntInclusive(1000, 1000000);
-    var offerType = TYPE_LIST[getRandomIntInclusive(0, 2)];
-    var offerRooms = getRandomIntInclusive(1, 5);
-    var offerGuests = Math.random().toFixed(1) * 10;
-    var offerCheckin = CHECKIN_LIST[getRandomIntInclusive(0, 2)];
-    var offerCheckout = CHECKOUT_LIST[getRandomIntInclusive(0, 2)];
-    var offerFeatures = getRandomElements(FEATURES_LIST);
-    var locationX = getRandomIntInclusive(300, 900);
-    var locationY = getRandomIntInclusive(100, 500);
 
-    var newAd = new Ad(imgNum, offerTitle, offerPrice, offerType, offerRooms, offerGuests, offerCheckin, offerCheckout, offerFeatures, locationX, locationY);
+    var newAd = createAd({
+      imgNum: i > 9 ? String(i) : '0' + i,
+      offerTitle: TITLE_LIST[getRandomIntInclusive(0, TITLE_LIST.length - 1)],
+      offerPrice: getRandomIntInclusive(1000, 1000000),
+      offerType: TYPE_LIST[getRandomIntInclusive(0, 2)],
+      offerRooms: getRandomIntInclusive(1, 5),
+      offerGuests: getRandomIntInclusive(1, 10),
+      offerCheckin: CHECKIN_LIST[getRandomIntInclusive(0, 2)],
+      offerCheckout: CHECKOUT_LIST[getRandomIntInclusive(0, 2)],
+      offerFeatures: getRandomElements(FEATURES_LIST),
+      locationX: getRandomIntInclusive(300, 900),
+      locationY: getRandomIntInclusive(100, 500),
+    });
 
     adList.push(newAd);
   }
@@ -39,27 +62,31 @@ function generateAds() {
 
 function generateAdsHTML(list) {
   var fragment = document.createDocumentFragment();
-  var markerWidth = 56;
-  var marketHeight = 75;
 
   for (var i = 0; i < list.length; i++) {
-    var adElement = document.createElement('div');
-    adElement.className = 'pin';
-    adElement.style.left = (list[i].location.x - markerWidth / 2) + 'px';
-    adElement.style.top = (list[i].location.y - marketHeight) + 'px';
+    var adElement = createAdElement(list[i], MARKER_WIDTH, MARKER_HEIGHT);
 
-    var adElementImg = document.createElement('img');
-    adElementImg.src = list[i].author.avatar;
-
-    adElement.appendChild(adElementImg);
     fragment.appendChild(adElement);
   }
 
   return fragment;
 }
 
-function appendAds(ads) {
-  var map = document.querySelector(MAP_CLASS);
+function createAdElement(elementData, markerWidth, markerHeight) {
+  var element = document.createElement('div');
+  var elementImg = document.createElement('img');
+
+  element.className = 'pin';
+  element.style.left = (elementData.location.x - markerWidth / 2) + 'px';
+  element.style.top = (elementData.location.y - markerHeight) + 'px';
+  elementImg.src = elementData.author.avatar;
+  element.appendChild(elementImg);
+
+  return element;
+}
+
+function appendAds(ads, mapClass) {
+  var map = document.querySelector('.' + mapClass);
 
   map.appendChild(ads);
 }
@@ -69,73 +96,87 @@ function generateLodge(featuredItem) {
   var lodgeMockup = document.querySelector('#offer-dialog');
   var lodge = template.content.cloneNode(true);
 
-  lodge.querySelector('.lodge__title').textContent = featuredItem.offer.title;
-  lodge.querySelector('.lodge__address').textContent = featuredItem.offer.address;
-  lodge.querySelector('.lodge__price').innerHTML = featuredItem.offer.price + '&#x20bd;/ночь';
-  lodge.querySelector('.lodge__type').textContent = translateType(featuredItem.offer.type);
-  lodge.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + featuredItem.offer.guests + ' гостей в ' + featuredItem.offer.rooms + ' комнатах';
-  lodge.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + featuredItem.offer.checkin + ', выезд до ' + featuredItem.offer.checkout;
-  lodge.querySelector('.lodge__features').appendChild(getFormattedFeatures(featuredItem.offer.features));
-  lodge.querySelector('.lodge__description').textContent = featuredItem.offer.description;
+  fillLodgeElement(lodge, 'title', featuredItem.offer.title);
+  fillLodgeElement(lodge, 'address', featuredItem.offer.address);
+  fillLodgeElement(lodge, 'type', translateType(featuredItem.offer.type));
+  fillLodgeElement(lodge, 'rooms-and-guests', 'Для ' + featuredItem.offer.guests
+    + ' гостей в ' + featuredItem.offer.rooms + ' комнатах');
+  fillLodgeElement(lodge, 'checkin-time', 'Заезд после '
+    + featuredItem.offer.checkin + ', выезд до ' + featuredItem.offer.checkout);
+  /*
+   Не уверен на счет такого переноса параметров. Но а как быть? Они даже в
+    одну строку в переменную не помещаются.
+  */
+  fillLodgeElement(lodge, 'description', featuredItem.offer.description);
+
+  var lodgePrice = featuredItem.offer.price + '&#x20bd;/ночь';
+  lodge.querySelector('.lodge__price').innerHTML = lodgePrice;
+
+  var lodgeFeaturesIcons = generateFeaturesIcons(featuredItem.offer.features);
+  lodge.querySelector('.lodge__features').appendChild(lodgeFeaturesIcons);
+  /*
+   Не знаю насколько это нормально - создавать пару переменных, только потому,
+   что они не влазят в ширину строки. А если быть последовательеым и все
+    параметры в переменные загонять, то становиться грамоздко.
+   */
+
   lodgeMockup.replaceChild(lodge, lodgeMockup.querySelector('.dialog__panel'));
 }
 
-function Ad(avatarNum, title, price, type, rooms, guests, checkin, checkout, features, x, y) {
-  this.author = {
-    avatar: 'img/avatars/user{{' + avatarNum + '}}.png',
-  };
-  this.offer = {
-    title: title,
-    address: x + ', ' + y,
-    price: price,
-    type: type,
-    rooms: rooms,
-    guests: guests,
-    checkin: checkin,
-    checkout: checkout,
-    features: features,
-    description: '',
-    photos: [],
-  };
-  this.location = {
-    x: x,
-    y: y,
-  };
+function fillLodgeElement(lodge, element, content) {
+  var selector = '.lodge__' + element;
+
+  lodge.querySelector(selector).textContent = content;
 }
 
-// function getMarkerStyle(styleName) {
-//   var element = document.querySelector('.pin');
-//   var style = window.getComputedStyle(element);
-//   var value = parseInt(style.getPropertyValue(styleName));
-//
-//   return value;
-// }
+function createAd(adData) {
+
+  var ad = {
+    author: {
+      avatar: 'img/avatars/user{{' + adData.imgNum + '}}.png',
+    },
+    offer: {
+      title: adData.offerTitle,
+      address: adData.locationX + ', ' + adData.locationY,
+      price: adData.offerPrice,
+      type: adData.offerType,
+      rooms: adData.offerRooms,
+      guests: adData.offerGuests,
+      checkin: adData.offerCheckin,
+      checkout: adData.offerCheckout,
+      features: adData.offerFeatures,
+      description: '',
+      photos: [],
+    },
+    location: {
+      x: adData.locationX,
+      y: adData.locationY,
+    },
+  }
+
+  return ad;
+}
 
 function translateType(type) {
-  var translatedType;
 
-  switch (type) {
-    case 'flat':
-      translatedType = 'Квартира';
-      break;
-    case 'bungalo':
-      translatedType = 'Бунгало';
-      break;
-    case 'house':
-      translatedType = 'Дом';
-      break;
-  }
+  var typeDictionary = {
+    flat: 'Квартира',
+    bungalo: 'Бунгало',
+    house: 'Дом',
+  };
+
+  var translatedType = typeDictionary[type];
 
   return translatedType;
 }
 
-function getFormattedFeatures(featuresList) {
+function generateFeaturesIcons(features) {
   var fragment = document.createDocumentFragment();
 
-  for (var i = 0; i < featuresList.length; i++) {
+  for (var i = 0; i < features.length; i++) {
     var feature = document.createElement('span');
-    feature.classList.add('feature__image');
-    feature.classList.add('feature__image--' + featuresList[i]);
+    feature.classList.add('feature__image'); //createFeature
+    feature.classList.add('feature__image--' + features[i]);
     fragment.appendChild(feature);
   }
 
@@ -144,11 +185,10 @@ function getFormattedFeatures(featuresList) {
 
 function getRandomElements(list) {
   var randomList = [];
-  var counter = 0;
 
   for (var i = 0; i < list.length; i++) {
     if (Math.random() > 0.5) {
-      randomList[counter++] = list[i];
+      randomList.push(list[i]);
     }
   }
 
