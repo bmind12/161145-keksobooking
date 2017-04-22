f171c477ab327681eefb636be1aa749b517eb30a
Sergey R <ryabovs@yahoo.com>
2017-04-22 12:20:37 +0200

    Form module converted into IIFE

--- js/form.js
+++ js/form.js
@@ -1,63 +1,65 @@
 'use strict';
 
-var form = document.querySelector('.notice__form');
-var apartmentType = form.querySelector('#type');
-var pricePerNight = form.querySelector('#price');
-var roomNumber = form.querySelector('#room_number');
-var capacity = form.querySelector('#capacity');
-var checkinTime = form.querySelector('#time');
-var checkoutTime = form.querySelector('#timeout');
+(function () {
+  var form = document.querySelector('.notice__form');
+  var apartmentType = form.querySelector('#type');
+  var pricePerNight = form.querySelector('#price');
+  var roomNumber = form.querySelector('#room_number');
+  var capacity = form.querySelector('#capacity');
+  var checkinTime = form.querySelector('#time');
+  var checkoutTime = form.querySelector('#timeout');
 
-var syncValues = function (element, value) {
-  element.value = value;
-};
+  roomNumber.addEventListener('change', function () {
+    var rooms = Number(roomNumber.options[roomNumber.selectedIndex].value);
+    var noGuestsIndex = 1;
+    var forGuestsIndex = 0;
+    if (rooms === 1) {
+      capacity.options.selectedIndex = noGuestsIndex;
+    } else {
+      capacity.options.selectedIndex = forGuestsIndex;
+    }
+  });
 
-window.syncFileds(
+  form.addEventListener('invalid', function (evt) {
+    evt.target.classList.add('invalid-input');
+  }, true);
+
+  form.addEventListener('change', function (evt) {
+    var element = evt.target;
+    if (element.validity.valid && element.classList.contains('invalid-input')) {
+      element.classList.remove('invalid-input');
+    }
+  });
+
+  var syncValues = function (element, value) {
+    element.value = value;
+  };
+
+  window.syncFileds(
     checkinTime,
     checkoutTime,
     ['12', '13', '14'],
     ['12', '13', '14'],
     syncValues
-);
+  );
 
-window.syncFileds(
+  window.syncFileds(
     checkoutTime,
     checkinTime,
     ['12', '13', '14'],
     ['12', '13', '14'],
     syncValues
-);
+  );
 
-var syncValueWithMin = function (element, value) {
-  element.min = value;
-};
+  var syncValueWithMin = function (element, value) {
+    element.min = value;
+  };
 
-window.syncFileds(
+  window.syncFileds(
     apartmentType,
     pricePerNight,
     ['apartment', 'shack', 'palace'],
     [1000, 0, 10000],
     syncValueWithMin
-);
-
-roomNumber.addEventListener('change', function () {
-  var rooms = Number(roomNumber.options[roomNumber.selectedIndex].value);
-  var noGuestsIndex = 1;
-  var forGuestsIndex = 0;
-  if (rooms === 1) {
-    capacity.options.selectedIndex = noGuestsIndex;
-  } else {
-    capacity.options.selectedIndex = forGuestsIndex;
-  }
-});
-
-form.addEventListener('invalid', function (evt) {
-  evt.target.classList.add('invalid-input');
-}, true);
-
-form.addEventListener('change', function (evt) {
-  var element = evt.target;
-  if (element.validity.valid && element.classList.contains('invalid-input')) {
-    element.classList.remove('invalid-input');
-  }
-});
+  );
+})();
